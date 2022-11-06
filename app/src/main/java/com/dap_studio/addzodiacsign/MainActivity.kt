4a3dca0e3508327a48ff0e8a8c221cb177487c65
Danil_Android_Veldyasov <danil.veldyasov@gmail.com>
2022-11-06 15:54:16 +0600

    s

---
+++ app/src/main/java/com/dap_studio/addzodiacsign/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dap_studio.addzodiacsign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
